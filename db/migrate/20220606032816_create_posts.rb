9cb08993ea5104b93ea334dd4be2bc7b471ecdd9
Pedro <nebewu@gmail.com>
2022-06-06 00:50:42 -0300

    Customize post migration and migrate

--- db/migrate/20220606032816_create_posts.rb
+++ db/migrate/20220606032816_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.text :title
-      t.text :body
+      t.text :title, null: false, limit: 300
+      t.text :body, null: false
 
       t.timestamps
     end
