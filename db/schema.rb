51c5f3166c7e130786b7c665b603ff117f9f8b5c
Pedro <nebewu@gmail.com>
2022-06-07 20:27:53 -0300

    Add username attribute to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_07_223823) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_07_232010) do
   create_table "posts", force: :cascade do |t|
     t.text "title", limit: 300, null: false
     t.text "body", null: false
@@ -26,8 +26,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_07_223823) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", limit: 32, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
