3420043f78b52500b028728bab7aacf2eef47842
Pedro <nebewu@gmail.com>
2022-06-07 23:26:35 -0300

    Associate posts to user and add post model user_id validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true, length: { in: 1..300 }
   validates :body, presence: true
+  validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_07_232010) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_08_022126) do
   create_table "posts", force: :cascade do |t|
     t.text "title", limit: 300, null: false
     t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_07_232010) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
