a0f620a916f1766ab6cf5d2117ba8b3798918183
Pedro <nebewu@gmail.com>
2022-06-08 23:01:12 -0300

    Nullify deleted user posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, dependent: :nullify
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
