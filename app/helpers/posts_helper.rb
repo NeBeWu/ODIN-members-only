66e84b1ca193a0a3b864e5ab653c76707229fde4
Pedro <nebewu@gmail.com>
2022-06-16 20:15:55 -0300

    Commit to remote access

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,12 @@
 module PostsHelper
+  def time_passed(post)
+    time_difference = Time.zone.now - post.created_at
+    if time_difference < 3600
+      "#{(time_difference / 60).ceil}m"
+    elsif time_difference < 86_400
+      "#{(time_difference / 3600).ceil}h"
+    else
+      "#{post.created_at.strftime("%b")} #{post.created_at.day}"
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :nullify
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { in: 1..32 }
   validates :email, presence: true, uniqueness: true
 
   # Include default devise modules. Others available are:
